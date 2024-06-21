23881658623182f9f011c871fb8e9964c285fda1
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-06-21 19:11:45 -0300

    alterando_dado_recebido_do_'input'_e_criando_alteracao_em_tempo_real_ultilizando_'data_binding'

--- src/app.js
+++ src/app.js
@@ -2,7 +2,19 @@ const profissional = {
     data() {
         return {
             nome: "Luis henrique s f",
-            programador: "full stack"
+            programador: "full stack",
+            input_program: ""
+        }
+    },
+    methods: {
+        submitForm(e) {
+
+            e.preventDefault();
+
+            console.log(this.input_program);
+
+            this.programador = this.input_program;
+
         }
     }
 }
